a966cffe7e6c725af82d824ef4db0193f0cb195e
Siraj Raval <sirajraval1@gmail.com>
2016-06-26 11:02:29 -0700

    added comments

--- demo.py
+++ demo.py
@@ -2,13 +2,15 @@ from api.ai import Agent
 import json
 
 
-##
+#initialize the agent 
 agent = Agent(
-     '<subscription-key>',
-     '9c4ddb316ec54081adee6273b7a768e5',
-     '5b9406958d62462f8bb72dd3e63a9a57',
+ 	 '<subscription-key>',
+     '<client-access-token>',
+     '<developer-access-token>',
 )
 
+# actions defined in the API.AI console that fire locally when an intent is
+# recognized
 def saveType(flowerType):
 	print 'do something here'
 
@@ -21,15 +23,20 @@ def createOrder(address):
 def main():
 	user_input = ''
 
+	#loop the queries to API.AI so we can have a conversation client-side
 	while user_input != 'exit':
 
+		#parse the user input
 		user_input  = raw_input("me: ")
+		#query the console with the user input, retrieve the response
 		response = agent.query(user_input)
+		#parse the response
 		result = response['result']
 		fulfillment = result['fulfillment']
 
 		print 'bot: ' + fulfillment['speech']
 
+		#if an action is deteted, fire the appropriate function
 		if result['action'] == 'saveFlowerType':
 			saveType(user_input)
 		if result['action'] == 'saveColor':
